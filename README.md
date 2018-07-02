41ba2a8b06c9f2ae9adf98f47370f8bb33674aa8
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-07-02 09:47:01 +0900

    add gradle-project dir

---
+++ README.md
@@ -0,0 +1,4 @@
+Katalon Forum Discussion $7609 : Text analysis on the page
+=========
+
+https://forum.katalon.com/discussion/7609/text-analysis-on-the-page#latest
---
+++ gradle-project/src/test/groovy/com/kazurayam/ksbackyard/RegexMatchingCyrillicAlphabetSpec.groovy
@@ -0,0 +1,67 @@
+package com.kazurayam.ksbackyard
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+import spock.lang.Specification
+
+
+class RegexMatchingCyrillicAlphabetSpec extends Specification {
+
+    // fixture methods
+    def setupSpec() {}
+    def setup() {}
+    def cleanup() {}
+    def cleanupSpec() {}
+
+    // feature methods
+    def testMatchingLatinAlphabet() {
+        setup:
+        String s = ' IVANOV IVAN 123 '
+        Pattern p = Pattern.compile(/^[ ][A-Z0-9 ]+[ ]$/)
+        when:
+        Matcher m = p.matcher(s)
+        then:
+        m.matches()
+    }
+
+    def testMatchingAnyUNICODE() {
+        setup:
+        String s = 'ИВАНОВ'
+        Pattern p = Pattern.compile(/[\p{L}]+/)
+        when:
+        Matcher m = p.matcher(s)
+        then:
+        m.matches()
+    }
+    
+    def testMatchingAnyUNICODE_DIGITS_BLANK() {
+        setup:
+        String s = 'ИВАНОВ ИВАН 123'
+        Pattern p = Pattern.compile(/[\p{L|Nd}\s]+/)
+        when:
+        Matcher m = p.matcher(s)
+        then:
+        m.matches()
+    }
+
+    def testMatchingCyrillicAlphabet_simple() {
+        setup:
+        String s = 'ИВАНОВ'
+        Pattern p = Pattern.compile(/[\p{InCyrillic}]+/)
+        when:
+        Matcher m = p.matcher(s)
+        then:
+        m.matches()
+    }
+    
+    def testMatchingCyrillicAlphabet_withBlankAndNumber() {
+        setup:
+        String s = ' ИВАНОВ ИВАН 123 '
+        Pattern p = Pattern.compile(/[\s]*[\p{IsCyrillic}\d\s]+[\s]*/)
+        when:
+        Matcher m = p.matcher(s)
+        then:
+        m.matches()
+    }
+}
